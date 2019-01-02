1d3a507db521fdb5da24dea271a4b50a357edbd6
caoziyu <czyhandsome@foxmail.com>
2019-01-02 21:22:12 +0800

    RNG

--- src/main/scala/ink/czyhandsome/fpinscala/states/RNG.scala
+++ src/main/scala/ink/czyhandsome/fpinscala/states/RNG.scala
@@ -8,14 +8,14 @@ package ink.czyhandsome.fpinscala.states
 trait RNG {
   def nextInt: (Int, RNG)
 
-  def nextNonPositiveInt: (Int, RNG) = {
+  def nextNonNegativeInt: (Int, RNG) = {
     val (n, r) = nextInt
     val nn = if (n < 0) -(n + 1) else n
     (nn, r)
   }
 
   def nextDouble: (Double, RNG) = {
-    val (n, r) = nextNonPositiveInt
+    val (n, r) = nextNonNegativeInt
     (n.toDouble / (Int.MaxValue + 1), r)
   }
 
@@ -61,14 +61,70 @@ case class SimpleRNG(seed: Long) extends RNG {
 }
 
 object RNG {
+  type Rand[+A] = RNG => (A, RNG)
+
+  val int: Rand[Int] = _.nextInt
+
+  def unit[A](a: A): Rand[A] = (a, _)
+
+  def map[A, B](s: Rand[A])(f: A => B): Rand[B] = rng => {
+    val (a, rng2) = s(rng)
+    (f(a), rng2)
+  }
+
+  def map2[A, B, C](a: Rand[A], b: Rand[B])(f: (A, B) => C): Rand[C] = rng => {
+    val (aa, ra) = a(rng)
+    val (bb, rb) = b(ra)
+    (f(aa, bb), rb)
+  }
+
+  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] =
+  //    fs.foldRight(unit(List[A]()) { (f, r) =>
+  //      rng => (f(rng)._1 :: r(rng)._1, f(rng)._2)
+  //    }
+    fs.foldRight(unit(List[A]())) { (f, acc) => map2(f, acc) { _ :: _ } }
+
+  def flatMap[A, B](f: Rand[A])(g: A => Rand[B]): Rand[B] =
+    rng => {
+      val (a, r) = f(rng)
+      g(a)(r)
+    }
+
+  def nonNegativeLessThan(n: Int): Rand[Int] =
+    flatMap(_.nextInt) { i =>
+      val mod = i % n
+      if (i + (n - 1) - mod >= 0) unit(mod)
+      else nonNegativeLessThan(n)
+    }
+
+  val nonNegativeEven: Rand[Int] = {
+    val r: Rand[Int] = _.nextNonNegativeInt
+    map(r) { i => i - i % 2 }
+  }
+
+  def map_viaFlatMap[A, B](f: Rand[A])(g: A => B): Rand[B] =
+    flatMap(f) { a => unit(g(a)) }
+
+  def map2[A, B, C](a: Rand[A], b: Rand[B])(f: (A, B) => C): Rand[C] =
+    flatMap(a) { aa =>
+      flatMap(b) { bb =>
+        unit(f(aa, bb))
+      }
+    }
+
+  val double: Rand[Double] = map(_.nextNonNegativeInt) { it => it.toDouble / (Int.MaxValue + 1) }
+
   def main(args: Array[String]): Unit = {
     val rng = SimpleRNG(42)
-    println(rng)
-    val (r2, rng2) = rng.nextInt
-    println((r2, rng2))
-    val c3 = rng2.nextDouble
-    println(c3)
-    val c4 = c3._2.ints(3)
-    println(c4)
+    val r1: Rand[Int] = _.nextNonNegativeInt
+    val r2: Rand[Int] = _.nextInt
+    val r3: Rand[Int] = map(_.nextInt) {
+      case i if i > 0 => -(i - 1)
+      case i@_ => i
+    }
+    val r4 = map(r3) { _ * 2 }
+    val r5 = map(r4) { _ * 3 }
+    val r = sequence(List(r1, r2, r3, r4, r5))
+    println(r(rng))
   }
 }
