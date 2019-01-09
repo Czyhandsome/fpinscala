477c2455c2b19c847118dc318b6838ddffc416a3
caoziyu <czyhandsome@foxmail.com>
2019-01-09 09:30:16 +0800

    Prop

--- src/main/scala/ink/czyhandsome/fpinscala/scalatest/Prop.scala
+++ src/main/scala/ink/czyhandsome/fpinscala/scalatest/Prop.scala
@@ -9,6 +9,23 @@ import ink.czyhandsome.fpinscala.states.RNG
   * @author 曹子钰, 2019-01-06
   */
 case class Prop(run: (TestCases, RNG) => Result) {
+  self =>
+  def &&(p: Prop): Prop = Prop { (n, rng) =>
+    (self.run(n, rng), p.run(n, rng)) match {
+      case (Passed, Passed) => Passed
+      case (f@Falsified(_, _), Passed) => f
+      case (Passed, f@Falsified(_, _)) => f
+      case (Falsified(f1, s1), Falsified(f2, s2)) => Falsified(s"$f1\n$f2", s1 + s2)
+    }
+  }
+
+  def ||(p: Prop): Prop = Prop { (n, rng) =>
+    (self.run(n, rng), p.run(n, rng)) match {
+      case (_, Passed) => Passed
+      case (Passed, _) => Passed
+      case (Falsified(f1, s1), Falsified(f2, s2)) => Falsified(s"$f1\n$f2", s1 + s2)
+    }
+  }
 }
 
 object Prop {
