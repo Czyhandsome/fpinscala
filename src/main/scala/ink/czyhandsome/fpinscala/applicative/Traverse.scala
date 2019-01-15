fc0131ac1a2389b0d2ef0c3829c403fc8a7b2ae2
caoziyu <czyhandsome@foxmail.com>
2019-01-15 10:09:48 +0800

    练习14

--- src/main/scala/ink/czyhandsome/fpinscala/applicative/Traverse.scala
+++ src/main/scala/ink/czyhandsome/fpinscala/applicative/Traverse.scala
@@ -1,6 +1,5 @@
 package ink.czyhandsome.fpinscala.applicative
 
-import ink.czyhandsome.fpinscala.applicative.IdApp.Id
 import ink.czyhandsome.fpinscala.monad.Functor
 
 import scala.language.{higherKinds, implicitConversions, reflectiveCalls}
@@ -13,19 +12,28 @@ import scala.language.{higherKinds, implicitConversions, reflectiveCalls}
 trait Traverse[F[_]] extends Functor[F] {
   type Const[A, B] = A
 
-  def traverse[G[_] : Applicative, A, B](fa: F[A])(f: A => G[B]): G[F[B]] =
-    sequence(map(fa) { f })
+  def traverse[G[_] : Applicative, A, B](fa: F[A])(f: A => G[B]): G[F[B]]
+
+  //  = sequence(map(fa) { f })
 
   def sequence[G[_] : Applicative, A](fga: F[G[A]]): G[F[A]] =
     traverse(fga) { ga => ga }
 
-  override def map[A, B](fa: F[A])(f: A => B): F[B] = ???
+  // ********** 练习12.14 ********** //
+  type Id[D] = D
+  private implicit val idApp: Applicative[Id] = new Applicative[Id] {
+    override def unit[A](a: => A): Id[A] = a
+
+    override def map2[A, B, C](fa: Id[A], fb: Id[B])(f: (A, B) => C): Id[C] = f(fa, fb)
+  }
+
+  override def map[A, B](fa: F[A])(f: A => B): F[B] = traverse[Id, A, B](fa)(f)(idApp)
 }
 
 object Traverse {
   def listTraverse: Traverse[List] = new Traverse[List] {
-    override def sequence[G[_], A](fga: List[G[A]])(implicit G: Applicative[G]): G[List[A]] =
-      fga.foldRight(G.unit(List[A]())) { (g, a) => G.map2(g, a) { _ :: _ } }
+    override def traverse[G[_], A, B](fa: List[A])(f: A => G[B])(implicit G: Applicative[G]): G[List[B]] =
+      fa.foldLeft(G.unit(List[B]())) { (acc, a) => G.map2(f(a), acc) { _ :: _ } }
   }
 
   def listApp: Applicative[List] = new Applicative[List] {
@@ -38,12 +46,14 @@ object Traverse {
   }
 
   def main(args: Array[String]): Unit = {
+    import IdApp.{Id => ID}
     val G = listApp
-    val T = new Traverse[Id] {
-      override def sequence[G[_], A](fga: Id[G[A]])(implicit G: Applicative[G])
-      : G[Id[A]] = G.map2(fga.value, G.unit(())) { (g, _) => Id(g) }
+    val T = new Traverse[ID] {
+      override def traverse[G[_], A, B](fa: ID[A])(f: A => G[B])(implicit g: Applicative[G]): G[ID[B]] =
+        g.map(f(fa.value)) { ID(_) }
     }
-    val r = T.sequence(Id(List(1, 2, 3)))(G)
+    val r = T.sequence(ID(List(1, 2, 3)))(G)
     println(r)
+    println(T.map(ID(1)) { "Hello " + _ })
   }
 }
