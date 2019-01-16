bd534788cf9d8deaca306d8f3f54f03abaf5cc78
caoziyu <czyhandsome@foxmail.com>
2019-01-16 09:37:05 +0800

    Traverse

--- src/main/scala/ink/czyhandsome/fpinscala/applicative/Traverse.scala
+++ src/main/scala/ink/czyhandsome/fpinscala/applicative/Traverse.scala
@@ -12,11 +12,11 @@ import scala.language.{higherKinds, implicitConversions, reflectiveCalls}
   * @author 曹子钰, 2019-01-13
   */
 trait Traverse[F[_]] extends Functor[F] with Foldable[F] {
-  def traverse[G[_] : Applicative, A, B](fa: F[A])(f: A => G[B]): G[F[B]]
+  def traverse[G[_], A, B](fa: F[A])(f: A => G[B])(implicit G:Applicative[G]): G[F[B]]
 
   //  = sequence(map(fa) { f })
 
-  def sequence[G[_] : Applicative, A](fga: F[G[A]]): G[F[A]] =
+  def sequence[G[_], A](fga: F[G[A]])(implicit G:Applicative[G]): G[F[A]] =
     traverse(fga) { ga => ga }
 
   // ********** 练习12.14 ********** //
